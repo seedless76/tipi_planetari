22ec9f04621304567cf3e75b5867f114345d784d
seedless76 <71174340+seedless76@users.noreply.github.com>
2020-10-12 20:02:24 +0200

    Versione del 11/10 ore 18.02 Finita la parte di calcolo e di visualizzazione delle dominanti!

--- lib/graphic/graphic_constants.dart
+++ lib/graphic/graphic_constants.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 const mainColor = Color(0xFF3C87E5);
-const kSignColor = Color(0xFF2c9e9a);
-const kBarColor = Color(0xFF2c9e9a);
+const kSignColor = Color(0xFF4C54FF);
+const kBarColor = Color(0xFF4C54FF);
 const kBackgroundBarColor = Color(0x775a6370);
-const kTileBackgroundColor = Color(0x7725282d);
-const kPageBackgroundColor = Color(0xFF121517);
+const kTileBackgroundColor = Color(0x7714141F);
+const kPageBackgroundColor = Color(0xFF202029);
 // 0xFF111344
 
 const kDominantNrStyle = TextStyle(

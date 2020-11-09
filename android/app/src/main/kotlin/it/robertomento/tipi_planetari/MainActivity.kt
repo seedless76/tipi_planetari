3a7c57de3e9e9adba4bd07e0e8903a03017832b4
seedless76 <71174340+seedless76@users.noreply.github.com>
2020-11-09 12:37:43 +0100

    Added web specific support: Google maps java API SVG web drawing via Image.Network

--- android/app/src/main/kotlin/it/robertomento/tipi_planetari/MainActivity.kt
+++ android/app/src/main/kotlin/it/robertomento/tipi_planetari/MainActivity.kt
@@ -4,3 +4,12 @@ import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
 }
+val crashButton = Button(this)
+crashButton.text = "Crash!"
+crashButton.setOnClickListener {
+    throw RuntimeException("Test Crash") // Force a crash
+}
+
+addContentView(crashButton, ViewGroup.LayoutParams(
+ViewGroup.LayoutParams.MATCH_PARENT,
+ViewGroup.LayoutParams.WRAP_CONTENT))
